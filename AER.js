249e5c1d972219f46ed28923a59b439e977857e4
Backds55 <luihiroshi6@gmail.com>
2022-12-07 20:48:10 -0300

    Add files via upload

---
+++ AER.js
@@ -0,0 +1,14 @@
+function AER(){
+    var status = document.getElementById("status").innerHTML;
+    if(status === "0"){
+        document.getElementById("status").innerHTML="1";
+        document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
+        document.getElementById("button").innerHTML="<a href='/?desligar' class='button'>Desligar Rele</a>";
+    } 
+    else{
+        document.getElementById("status").innerHTML="0";
+        document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
+        document.getElementById("button").innerHTML="<a href='/?ligar' class='button'>Ligar Rele</a>";
+    }
+}
+//by Luis
