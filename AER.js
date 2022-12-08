81dd8ccd05fc55182cc0acc6abf160bfdd319d4c
Backds55 <luihiroshi6@gmail.com>
2022-12-07 21:36:54 -0300

    Update AER.js

--- AER.js
+++ AER.js
@@ -1,14 +1,12 @@
 function AER(){
-    var status = document.getElementById("status").innerHTML;
-    if(status === "0"){
-        document.getElementById("status").innerHTML="1";
-        document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
-        document.getElementById("button").innerHTML="<a href='/?desligar' class='button'>Desligar Rele</a>";
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "0"){
+        document.getElementById("estado").innerHTML="1";
+        document.getElementById("botao").innerHTML="<a href='/?desligar'>Desligar Rele</a>";
     } 
     else{
-        document.getElementById("status").innerHTML="0";
-        document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
-        document.getElementById("button").innerHTML="<a href='/?ligar' class='button'>Ligar Rele</a>";
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("botao").innerHTML="<a href='/?ligar'>Ligar Rele</a>";
     }
 }
 //by Enrico, Jonatan e Luis - Microcontroladores
